5cf55f1c2c55b778f67ad92d2176bb47b7a30fc0
James Wood <jamie_wood@ymail.com>
2014-12-01 13:25:26 +0000

    Final build

---
+++ Unity Project/Assets/Scripts/CatScare.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatScare : MonoBehaviour 
+{
+	public int scareTimer = 0;
+	public int scareLimit = 30 * 60;
+	public bool scare = false;
+
+	public int lightTimer = 0;
+	public int lightLimit = 5 * 60;
+	public bool light = true;
+
+	public GUIText text;
+	public GUIText sleepText;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (!light)
+		{
+			Light[] lights = FindObjectsOfType(typeof(Light)) as Light[];
+
+			foreach (Light flashlight in lights)
+			{
+				if (flashlight.tag == "flashlight")
+				{
+					flashlight.enabled = false;
+				}
+			}
+		}
+
+		if (light)
+		{
+			Light[] lights = FindObjectsOfType(typeof(Light)) as Light[];
+			
+			foreach (Light flashlight in lights)
+			{
+				if (flashlight.tag == "flashlight")
+				{
+					flashlight.enabled = true;
+				}
+			}
+		}
+
+		if (!scare)
+		{
+			scareTimer++;
+
+			if (scareTimer > scareLimit)
+			{
+				scare = true;
+				renderer.enabled = true;
+				light = false;
+			}
+		}
+
+		if (scare && !light)
+		{
+			lightTimer++;
+
+			if (lightTimer > lightLimit)
+			{
+				light = true;
+				text.enabled = true;
+				sleepText.text = "Thank you for playing - ESC to quit";
+			}
+		}
+	}
+}
---
+++ Unity Project/Assets/Scripts/Fade.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fade : MonoBehaviour 
+{
+	public bool fading;
+	public bool faded;
+	public int fadeTimer;
+	private int fadeLimit = 180;
+	private float fadeIncrement = 1 / 180.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		fadeTimer = 0;
+		faded = false;
+		fading = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (!fading)
+		{
+			fadeTimer++;
+
+			if (fadeTimer > fadeLimit)
+			{
+				fadeTimer = 0;
+				fading = true;
+			}
+		}
+
+		else if (!faded && fading)
+		{
+			renderer.material.SetColor("_Color", new Color(1.0f, 1.0f, 1.0f, renderer.material.color.a - fadeIncrement));
+		
+			if (renderer.material.color.a <= 0.01f)
+			{
+				faded = true;
+				fading = false;
+			}
+		}
+
+		else if (faded && fading)
+		{
+			renderer.material.SetColor("_Color", new Color(1.0f, 1.0f, 1.0f, renderer.material.color.a + fadeIncrement));
+
+			if (renderer.material.color.a >= 0.99f)
+			{
+				faded = false;
+				fading = false;
+			}
+		}
+
+	}
+}
